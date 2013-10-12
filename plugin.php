8ce381a0782238035abf73be25fc94ef8fa2478a
Leho Kraav <leho@kraav.com>
2013-10-12 00:10:05 -0400

    account for push parameter in unit calculations

--- plugin.php
+++ plugin.php
@@ -65,6 +65,8 @@ class Pure_CSS {
     static function gc_column_class( $classes, $attr ) {
         extract( $attr );
 
+        if ( $push ) $span += $push;
+
         if ( $grid % $span === 0 ) {
             $grid = $grid / $span;
             $span = 1;
