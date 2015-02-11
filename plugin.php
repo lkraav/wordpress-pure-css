ee162954d46a5e5e94cf45011e5128627e545a16
leho <leho@kraav.com>
2015-02-11 22:42:51 +0200

    hello pure 0.6.0

--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@ Pure_CSS::on_load();
 class Pure_CSS {
     static $version = "1.5.1"; # TODO get_plugin_data()
 
-    static $pure_css_version = "0.5.0"; #
+    static $pure_css_version = "0.6.0"; #
     static $pure_css_minified = true; #
 
     # http://purecss.io/customize/
